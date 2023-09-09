94d616b2b580a6a2d082bc6a60659a07d5c51965
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-08 18:35:32 -0700

    added weather api root and key, dom els, dayjs

---
+++ App.js
@@ -0,0 +1,17 @@
+// Constants for the app
+const WEATHER_API_ROOT_URL = 'https://api.openweathermap.org';
+const WEATHER_API_KEY = '';
+
+const HISTORY_KEY = 'search-history';
+let searchHistoryList = [];
+
+// DOM elements
+const searchFormEl = document.querySelector('#search-form');
+const searchInputEl = document.querySelector('#search-input');
+const currentWeatherEl = document.querySelector('#today');
+const forecastEl = document.querySelector('#forecast');
+const searchHistoryEl = document.querySelector('#history');
+
+// dayjs plugin
+dayjs.extend(dayjs_plugin_utc);
+dayjs.extend(dayjs_plugin_timezone);
